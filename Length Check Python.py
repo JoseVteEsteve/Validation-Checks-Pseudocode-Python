45952670b79616ae68fd0f71a8041b5ba0918741
JoseVteEsteve <39584994+JoseVteEsteve@users.noreply.github.com>
2020-02-27 19:25:52 +0100

    Create Length Check Python.py

---
+++ Length Check Python.py
@@ -0,0 +1,16 @@
+#length check implemented in Python
+
+lower = int(input("Enter the lower bound:") 
+upper =int(input ("Enter the upper blund:")) 
+
+flag = True
+while flag + True:
+  number = float(input("Enter a number:")) 
+  if number >= lower AND number <= upper:
+    print("The number is accepted") 
+    flag = False
+  else:
+    print("The number is not accepted") 
+    
+    
+  
