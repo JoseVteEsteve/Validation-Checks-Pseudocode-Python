5d433a7d549f47d728ad2fb3f4ec9c162b2ae7d6
JoseVteEsteve <39584994+JoseVteEsteve@users.noreply.github.com>
2020-02-27 19:27:06 +0100

    Update Length Check Python.py

--- Length Check Python.py
+++ Length Check Python.py
@@ -1,10 +1,10 @@
 #length check implemented in Python
 
 lower = int(input("Enter the lower bound:") 
-upper =int(input ("Enter the upper blund:")) 
+upper = int(input ("Enter the upper blund:")) 
 
 flag = True
-while flag + True:
+while flag = True:
   number = float(input("Enter a number:")) 
   if number >= lower AND number <= upper:
     print("The number is accepted") 
